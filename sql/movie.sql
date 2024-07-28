d2f1f1fe2b0d581cb3c4784882185c98a3b0f7b5
parkyejin <uiop5809@naver.com>
2024-07-29 00:43:16 +0900

    remove: sql table 삭제

--- sql/movie.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE movie (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    poster_url VARCHAR(255),
-    overview TEXT,
-    vote_average DOUBLE,
-    release_date VARCHAR(255)
-);
