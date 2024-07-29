28de65a854e91948657063d1e6dcc3fbd02e7f83
parkyejin <uiop5809@naver.com>
2024-07-29 09:40:17 +0900

    docs: TMDB API key 분리

--- src/main/java/com/spring/movie_mingle/service/MovieService.java
+++ src/main/java/com/spring/movie_mingle/service/MovieService.java
@@ -3,6 +3,7 @@ package com.spring.movie_mingle.service;
 import com.spring.movie_mingle.domain.Movie;
 import com.spring.movie_mingle.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,12 +16,14 @@ public class MovieService {
 
     private final MovieRepository movieRepository;
 
-    private final String TMDB_API_KEY = "0e1a71e289465f90209535b3a6039dc2";
-    private final String TMDB_API_URL = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ko-KR&page=1&sort_by=popularity.desc&api_key=" + TMDB_API_KEY;
+    @Value("${tmdb.api.key}")
+    private String tmdbApiKey;
+
+    private final String TMDB_API_URL = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ko-KR&page=1&sort_by=popularity.desc&api_key=";
 
     public String insertMovie() {
         RestTemplate restTemplate = new RestTemplate();
-        Map<String, Object> response = restTemplate.getForObject(TMDB_API_URL, Map.class);
+        Map<String, Object> response = restTemplate.getForObject(TMDB_API_URL + tmdbApiKey, Map.class);
 
         @SuppressWarnings("unchecked")
         List<Map<String, Object>> results = (List<Map<String, Object>>) response.get("results");
@@ -45,6 +48,4 @@ public class MovieService {
     public Movie findById(Long id) {
         return movieRepository.findById(id).orElseThrow();
     }
-
-
 }
